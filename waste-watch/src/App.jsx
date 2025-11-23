5dde8738da1e2075dfed13cecff5dfa78bbcfd2a
C-Adam <adamcooper427@gmail.com>
2025-11-23 03:28:00 -0500

    dwaydub

--- waste-watch/src/App.jsx
+++ waste-watch/src/App.jsx
@@ -2,17 +2,16 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./components/navbar";
 
 function App() {
-    const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0);
 
-    return (
-        <>
-            <Navbar />
-        </>
-    );
+  return (
+    <>
+      <h1>Helolo my </h1>
+    </>
+  );
 }
 
 export default App;
