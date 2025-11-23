88c6c1dbf44bb47b204f025953ea03e8f0d2c765
C-Adam <adamcooper427@gmail.com>
2025-11-23 03:24:06 -0500

    sabdaybwd

--- iot/start_cam.py
+++ iot/start_cam.py
@@ -1,19 +1,23 @@
 from picamera2 import Picamera2
-import cv2, time
+import cv2
+from ultralytics import YOLO
 
 picam2 = Picamera2()
-
-config = picam2.create_preview_configuration(main={"format": "RGB888"})
+config = picam2.create_preview_configuration(main={"format": 'RGB888', "size": (1000, 600)})
 picam2.configure(config)
-
 picam2.start()
-time.sleep(1)
+
+model = YOLO("yolov8n.pt")
 
 while True:
     frame = picam2.capture_array()
-    cv2.imshow("Camera Preview", frame)
 
+    results = model(frame)
+    annotated_frame = results[0].plot()
+    # Process the frame (for example, display or analyze)
+    cv2.imshow("YOLO Camera", annotated_frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
+
 cv2.destroyAllWindows()
 picam2.stop()
